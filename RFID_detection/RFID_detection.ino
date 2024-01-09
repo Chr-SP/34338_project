edf9328f7964c428c4ff8fc8569c03d6ec8bd596
Chr-SP <christian@saxoharpoter.dk>
2024-01-09 11:50:33 +0100

    Better library

--- RFID_detection/RFID_detection.ino
+++ RFID_detection/RFID_detection.ino
@@ -1,3 +1,14 @@
+#include <SPI.h>
+#include <MFRC522.h> 
+
+#define RST_PIN         9                                               // set Reset to digital pin 9
+#define SS_PIN          10                                              // set SDA to digital pin 10
+
+MFRC522 rfcreader(SS_PIN, RST_PIN);
+
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
