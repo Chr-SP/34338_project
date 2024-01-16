aab51bfe3754f781b9103787b1536cbc176c329b
S173929 <s173929@student.dtu.dk>
2024-01-16 10:35:36 +0100

    Master_Jesper_Root_text

--- Master_v4/Master_v4.ino
+++ Master_v4/Master_v4.ino
@@ -19,8 +19,8 @@ const char* server_password = "Anna1234";
 const int lightSensorPin = A0;
 const int motionSensorIndoorPin = D0;
 const int motionSensorOutdoorPin = D3;
-const int RST_PIN = D4;    // set Reset to digital pin 9
-const int SS_PIN = D8;    // set SDA to digital pin 10
+const int RST_PIN = D4;  // set Reset to digital pin 9
+const int SS_PIN = D8;   // set SDA to digital pin 10
 
 const int LOCKDOOR = 0;
 const int OPENDOOR = 1;
@@ -31,8 +31,8 @@ const int ALARM_OFF = 0;
 const int LIGHT_ON = 1;
 const int LIGHT_OFF = 0;
 
-uint8_t keypad_RFID_select = RFID_ON;   // control variable to switch between RFID and keypad (1 fot RFID, 0 for keypad)
-uint8_t alarm_on_off = 0;  // control variable for the alarm (0 for off)
+uint8_t keypad_RFID_select = RFID_ON;  // control variable to switch between RFID and keypad (1 fot RFID, 0 for keypad)
+uint8_t alarm_on_off = 0;              // control variable for the alarm (0 for off)
 
 const char INDOOR_LED = 'a';
 const char OUTDOOR_LED = 'b';
@@ -40,8 +40,8 @@ const char SERVOCONTROL = 'c';  // Servo destination?
 const char ALARMCONTROL = 'd';
 
 
-int lightThreshold = 160;  //A threshold that controls when light level is low
-int lockPosition = OPENDOOR;      // position of the servo / lock
+int lightThreshold = 160;     //A threshold that controls when light level is low
+int lockPosition = OPENDOOR;  // position of the servo / lock
 
 
 char toSend[3] = { 0, 0 };  // the char defining a command to send to slave
@@ -62,8 +62,11 @@ unsigned long timestamp;
 /* Variable text strings used in HTML code*/
 String door_text = " Door is open";
 String RFID_text = " RFID is active";
-String user_text = " User 1 is active";
 String alarm_text = " Alarm is off";
+String user1_text = " has accsess";
+String user2_text = " has accsess";
+String user3_text = " has accsess";
+
 
 char password[4] = { 0, 0, 0 };  // Password for keypad
 /* Handle functions for the server*/
@@ -79,15 +82,15 @@ void handle_alarm();
 void handle_password();
 
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); //Define a new RFC reader
-MFRC522::MIFARE_Key key; //Defines a new instanse of the MIFARE key
-MFRC522::StatusCode status; //Defines an instanse of the status code
+MFRC522 mfrc522(SS_PIN, RST_PIN);  //Define a new RFC reader
+MFRC522::MIFARE_Key key;           //Defines a new instanse of the MIFARE key
+MFRC522::StatusCode status;        //Defines an instanse of the status code
 //Matrix contaning ID's defined as valid
-byte validAccess[4][4] = {{0x63, 0xC8, 0xA0, 0x34},
-                          {0xF9, 0xAD, 0xD8, 0x15},
-                          {0xB9, 0xE1, 0x6C, 0x14},
-                          {0x53, 0xB2, 0x05, 0x34}};
-byte userAccess[4] = {1, 1, 1, 1};  // users who have access              
+byte validAccess[4][4] = { { 0x63, 0xC8, 0xA0, 0x34 },
+                           { 0xF9, 0xAD, 0xD8, 0x15 },
+                           { 0xB9, 0xE1, 0x6C, 0x14 },
+                           { 0x53, 0xB2, 0x05, 0x34 } };
+byte userAccess[4] = { 1, 1, 1, 1 };  // users who have access
 bool access;
 char name[18];
 
@@ -121,25 +124,25 @@ void setup() {
   init_sever_connection();
 
   SPI.begin();
-  mfrc522.PCD_Init(); //Setup and initialize RFID reader
+  mfrc522.PCD_Init();  //Setup and initialize RFID reader
   //Prepare the keys for authentication
-  for(int i = 0 ; i < 6 ; i++){
+  for (int i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
 }
 
 void loop() {
-  char toSend[3] = {0,0};
+  char toSend[3] = { 0, 0 };
 
   server.handleClient();
 
-  
+
   // Check lighting
   Serial.println(digitalRead(motionSensorOutdoorPin));
 
   lightsystemIndoor(lockPosition);
   lightsystemOutdoor();
-  
+
   motionAlarm(lockPosition);
 
   readID(&access, &name[0]);
@@ -201,15 +204,15 @@ void getMessage() {
 
     if ((recieved[1]) && (doClear == 0)) {  // when a button is pressed
 
-      if (recieved[1] == '*') {             // delete all button
+      if (recieved[1] == '*') {  // delete all button
         cursorPosition = 0;
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Enter password: ");
-        memset(inputPassword, 0, sizeof(inputPassword));      // reset password
+        memset(inputPassword, 0, sizeof(inputPassword));  // reset password
 
-        
-      } else if (recieved[1] == '#') {                                // when # button is pressed
+
+      } else if (recieved[1] == '#') {                        // when # button is pressed
         if (cursorPosition == 3) {                            // when # button is pressed as botton number 4 check if password is true
           if (memcmp(inputPassword, truePassword, 4) == 0) {  // password is true
             lcd.setCursor(0, 1);
@@ -219,7 +222,7 @@ void getMessage() {
             lcd.setCursor(0, 1);
             lcd.print("Wrong password");
           }
-        } else if (cursorPosition >= 3) {                   //if more than 3 buttons are pressed the password is wrong
+        } else if (cursorPosition >= 3) {  //if more than 3 buttons are pressed the password is wrong
           lcd.setCursor(0, 1);
           lcd.print("Wrong password");
         } else {  // when there is not enough numbers entered in the code
@@ -238,7 +241,7 @@ void getMessage() {
         memset(inputPassword, 0, sizeof(inputPassword));  // Reset password
         doClear = 1;
         timestamp = millis();
-        
+
       } else {  // add number to array and print on lcd at the same time
         inputPassword[cursorPosition] = recieved[1];
         cursorPosition++;
@@ -256,7 +259,7 @@ void lightsystemIndoor(int lockPosition) {
   if ((lightLevel > lightThreshold) || (!digitalRead(motionSensorIndoorPin))) {  //when there is light (light level is higher than threshold), then turn off LED
     slaveControlWord(INDOOR_LED, LIGHT_OFF);
   } else if ((lockPosition == 0) && (digitalRead(motionSensorIndoorPin))) {  // If it is dark (light level is less than threshold) turn on LED an amout dependet on how dark it is.
-    int light = 255 - lightLevel * 254 / lightThreshold;   // SKAL SKALERES YDERLIGERE
+    int light = 255 - lightLevel * 254 / lightThreshold;                     // SKAL SKALERES YDERLIGERE
     slaveControlWord(INDOOR_LED, light);
   }
 }
@@ -271,8 +274,8 @@ void lightsystemOutdoor() {
   }
 }
 
-void motionAlarm(int lockPosition){ 
-  if((!lockPosition == LOCKDOOR) && (digitalRead(motionSensorIndoorPin))){ //If the door is locked and there is motion, the alarm starts
+void motionAlarm(int lockPosition) {
+  if ((!lockPosition == LOCKDOOR) && (digitalRead(motionSensorIndoorPin))) {  //If the door is locked and there is motion, the alarm starts
     handle_alarm();
   }
 }
@@ -306,22 +309,23 @@ void handleRoot() {  // When URI / is requested, send a web page with a button t
       <form action=\"/ALARMON\" method=\"POST\" ><input type=\"submit\" value=\"Alarm on\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\";<p>"
                                   + alarm_text + " <p></form> \
       <p>Person RFID access<p>\
-      <form action=\"/User1\" method=\"POST\" ><input type=\"submit\" value=\"User1\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"></form> \
-      <form action=\"/User2\" method=\"POST\" ><input type=\"submit\" value=\"User2\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"></form> \
-      <form action=\"/User3\" method=\"POST\" ><input type=\"submit\" value=\"User3\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"></form> \
+      <form action=\"/User1\" method=\"POST\" ><input type=\"submit\" value=\"User1\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"; <p>" 
+                                  + user1_text +"<p></form> \
+      <form action=\"/User2\" method=\"POST\" ><input type=\"submit\" value=\"User2\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"; <p>" 
+                                  + user2_text +"<p></form> \
+      <form action=\"/User3\" method=\"POST\" ><input type=\"submit\" value=\"User3\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"; <p>" 
+                                  + user3_text +"<p></form> \
       <form action=\"/User4\" method=\"POST\" ><input type=\"submit\" value=\"User4\" style=\"width:60px; height:20px; font-size:10px; background-color: #ff88cc; border-color: ##ff0080\"></form> \
-      <p>" + user_text + "<p>\
       <p>Change keypad code<p>\
       <form action =\"/PASSWORD\" method=\"POST\"><input type=\"password\" name=\"password\" placeholder=\"Password\"></br><input type=\"submit\" value=\"Change password\"></form> \
       </body></html>");
 }
-// <form action=\"/PASSWORD\" method=\"POST\"><input type=\"text\" name=\"password\" placeholder=\"New password\"></form>
 void server_update_header() {
   server.sendHeader("Location", "/");  // Add a header to respond with a new location for the browser to go to the home page again
   server.send(303);                    // Send it back to the browser with an HTTP status 303 (See Other) to redirect
 }
 
-void handle_door() {  // If a POST request is made to URI /LED
+void handle_door() {   // If a POST request is made to URI /LED
   if (lockPosition) {  // Lock door request
     slaveControlWord(SERVOCONTROL, LOCKDOOR);
     door_text = " Door is locked";
@@ -344,7 +348,7 @@ void handle_RFID_keypad() {  // If a POST request is made to URI /LED
 }
 void handle_user1() {
   user_text = " User1 has access";
-  userAccess[4] = {1, 0, 0, 0};  // users who have access              
+  userAccess[4] = { 1, 0, 0, 0 };  // users who have access
 
   server_update_header();
 }
@@ -359,7 +363,7 @@ void handle_user3() {
 void handle_user4() {
   user_text = " User4 has access";
   server_update_header();
-  userAccess[4] = {1, 1, 1, 1};
+  userAccess[4] = { 1, 1, 1, 1 };
 }
 
 void handle_alarm() {  // If a POST request is made to URI /LED
@@ -383,10 +387,10 @@ void handle_password() {                                               // If a P
     return;
   }
   // Check for password is 3 ing length
-  if ((server.arg("password").length()) != 3) {                                                        // If the POST request doesn't have a password data
-    server.send(400, "text/plain", "400: Invalid! Password has to have 3 characters long");            // The request is invalid, so send HTTP status 400
+  if ((server.arg("password").length()) != 3) {                                              // If the POST request doesn't have a password data
+    server.send(400, "text/plain", "400: Invalid! Password has to have 3 characters long");  // The request is invalid, so send HTTP status 400
     return;
-  }else if (server.arg("password")) {                                                                  // If both the username and the password are correct
+  } else if (server.arg("password")) {  // If both the username and the password are correct
     server.send(200, "text/html", "<h1>Your new password is, " + server.arg("password") + "!</h1><p>Password change successful</p>");
     from_String_to_CharArray(server.arg("password"), truePassword);
   } else {  // Username and password don't match
@@ -435,29 +439,29 @@ void init_sever_connection() {
 }
 
 
-void readID(bool *a, char* n){
+void readID(bool* a, char* n) {
   byte UID[4];
-  
-  // Reset the loop if no new card present on the sensor/reader
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-		return;
-	}
 
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
-		return;
-	}
-  
+  // Reset the loop if no new card present on the sensor/reader
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
   //Read all values of the UID from a card and store them in UID array
-  for(int i = 0 ; i < 4; i++){
+  for (int i = 0; i < 4; i++) {
     UID[i] = mfrc522.uid.uidByte[i];
   }
 
 
   *a = checkAccess(&UID[0]);
 
-  
-  
-  if(access == true){
+
+
+  if (access == true) {
     readDataFromKey(n);
     lcd.setCursor(0, 1);
     lcd.print("Welcome home ");
@@ -468,98 +472,94 @@ void readID(bool *a, char* n){
   }
 
   //Print function for debugging.
-  
-  for(int i = 0 ; i < 4; i++){
-    Serial.print(UID[i],HEX);
+
+  for (int i = 0; i < 4; i++) {
+    Serial.print(UID[i], HEX);
     Serial.print(" ");
   }
   Serial.println(" Card has been read");
 
   Serial.println(name);
-  
 
-  mfrc522.PICC_HaltA(); //Prevents redetection of a card
+
+  mfrc522.PICC_HaltA();  //Prevents redetection of a card
 
   mfrc522.PCD_StopCrypto1();
-
 }
 
 //Checks all stored card numbers with UID, if there is a match return acces granted as true
-bool checkAccess(byte *UID){
+bool checkAccess(byte* UID) {
   bool accessGranted = false;
-  
-  for(int i = 0 ; i < 4 ; i++){ //Runs through all stored cards
+
+  for (int i = 0; i < 4; i++) {  //Runs through all stored cards
     int match = 0;
-    for(int k = 0 ; k < 4 ; k++){ //Runs through all numbers in a saved card
-      if (validAccess[i][k] == UID[k]){ 
+    for (int k = 0; k < 4; k++) {  //Runs through all numbers in a saved card
+      if (validAccess[i][k] == UID[k]) {
         match++;
       }
     }
-    if((match == 4) && (userAccess[i])){
+    if ((match == 4) && (userAccess[i])) {
       accessGranted = true;
     }
   }
   return accessGranted;
 }
 
-void writeDataToKey(char initials[2]){
+void writeDataToKey(char initials[2]) {
 
   // Reset the loop if no new card present on the sensor/reader
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-		return;
-	}
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
-		return;
-	}
-  
-  //3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63 
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  //3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63
   //IS OFF LIMITS AND WILL RUIN A SECTOR ON A CARD IF USED!
-  int blockNumber = 16; //Number of the block that will be written to
+  int blockNumber = 16;  //Number of the block that will be written to
 
   //Authentication check for writing
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNumber, &key, &(mfrc522.uid));
 
   //Check for success of authentication
-  if(status != MFRC522::STATUS_OK){
+  if (status != MFRC522::STATUS_OK) {
     //Serial.println("Error in authentication");
     return;
-  }
-  else{
+  } else {
     Serial.println("Authentication succesful");
   }
-  
 
-  
-  status = mfrc522.MIFARE_Write(blockNumber, (byte*)initials, 16); //Try to write data
+
+
+  status = mfrc522.MIFARE_Write(blockNumber, (byte*)initials, 16);  //Try to write data
 
   //Check for succes of write
-  if(status != MFRC522::STATUS_OK){
+  if (status != MFRC522::STATUS_OK) {
     Serial.println("Failed writing data");
     return;
-  } 
-  else{
+  } else {
     Serial.println("Data was written succesfully");
   }
-  
 
-  mfrc522.PICC_HaltA(); //Prevents reditection of a card
+
+  mfrc522.PICC_HaltA();  //Prevents reditection of a card
 
   mfrc522.PCD_StopCrypto1();
-
 }
 
-void readDataFromKey(char *n){
-  //3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63 
+void readDataFromKey(char* n) {
+  //3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63
   //IS OFF LIMITS AND WILL RUIN A SECTOR ON A CARD IF USED!
-  int blockNumber = 16; //Number of the block that will be written to
+  int blockNumber = 16;  //Number of the block that will be written to
   byte tempData[18];
-  byte bufferLength = 18; 
+  byte bufferLength = 18;
 
   //Authentication check for writing
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockNumber, &key, &(mfrc522.uid));
 
   //Check for success of authentication
-  if(status != MFRC522::STATUS_OK){
+  if (status != MFRC522::STATUS_OK) {
     //Serial.println("Error in authentication");
     return;
   }
@@ -570,11 +570,10 @@ void readDataFromKey(char *n){
   */
 
 
-  status = mfrc522.MIFARE_Read(blockNumber, tempData, &bufferLength); //Try to read from the wanted block and store it in tempData
+  status = mfrc522.MIFARE_Read(blockNumber, tempData, &bufferLength);  //Try to read from the wanted block and store it in tempData
   bufferLength = 18;
   //Check for succes of read
-  if (status != MFRC522::STATUS_OK)
-  {
+  if (status != MFRC522::STATUS_OK) {
     //Serial.print("Failed to read data");
     return;
   }
@@ -585,12 +584,8 @@ void readDataFromKey(char *n){
   */
 
   //Copy data from temp variable to wanted variable
-  for(int i = 0; i < 18; i++){
+  for (int i = 0; i < 18; i++) {
     *n = tempData[i];
     n++;
   }
 }
-
-
-
-
